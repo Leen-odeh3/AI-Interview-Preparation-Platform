da57a733694f1be3abfbeac80dc60df8cfd93d80
Leen Odeh <leenodeh287@gmail.com>
2025-03-14 13:37:01 +0200

    Connect front-end with back-end

--- react-frontend/src/component/Auth/Login.jsx
+++ react-frontend/src/component/Auth/Login.jsx
@@ -1,14 +1,28 @@
 import { MdAlternateEmail } from "react-icons/md";
-import { FaFingerprint } from "react-icons/fa";
-import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { FaFingerprint, FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { login } from '../../api/api';  
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const togglePasswordView = () => setShowPassword(!showPassword);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
   const navigate = useNavigate();
+
+  const togglePasswordView = () => setShowPassword(!showPassword);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await login(email, password);
+      navigate('/dashboard'); 
+    } catch (error) {
+      setError(error);
+    }
+  };
 
   const handleClick = () => {
     navigate('/register');
@@ -20,7 +34,7 @@ const Login = () => {
 
   return (
     <div className="w-full h-screen flex items-center justify-center hero ">
-      <div className="Main w-[90%] w-sm md:w-2/3 lg:w-[40%] p-5 flex-col flex items-center gap-3 rounded-xl shadow-slate-200 shadow-lg child">
+      <div className="Main w-[90%] sm:w-2/3 md:w-[40%] p-5 flex-col flex items-center gap-3 rounded-xl shadow-slate-200 shadow-lg child">
         <h3 className="text-secondary text-base font-bold mt-5">Sign in to ai-mock-interview</h3>
         <p className=" text-primary text-sm">Welcome back! Please sign in to continue</p>
         <div className="w-1/2 lg:w-2/3 flex gap-2">
@@ -28,22 +42,21 @@ const Login = () => {
             <AiFillGithub style={{ color: '#181717' }} className="text-lg md:text-xl" />
             <span className="text-sm md:text-base font-medium">GitHub</span>
           </div>
-
           <div className="p-2 md:px-6 lg:px-8 cursor-pointer rounded-xl flex items-center gap-2 icon__btn">
             <AiFillLinkedin style={{ color: '#0077B5' }} className="text-lg md:text-xl" />
             <span className="text-sm md:text-base font-medium">LinkedIn</span>
           </div>
         </div>
 
-        <h3 className="font-lora text-xs md:text-sm px-4 text-gray-500">
-          Or
-        </h3>
+        <h3 className="font-lora text-xs md:text-sm px-4 text-gray-500">Or</h3>
 
         <div className="w-2/3 flex flex-col gap-3">
           <div className="w-full flex items-center gap-2 p-2 rounded-xl icon__btn">
             <MdAlternateEmail />
             <input
               type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="Email address"
               className="bg-transparent border-0 w-full outline-none text-sm md:text-base"
             />
@@ -53,6 +66,8 @@ const Login = () => {
             <FaFingerprint />
             <input
               type={showPassword ? "password" : "text"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder="Password"
               className="bg-transparent border-0 w-full outline-none text-sm md:text-base"
             />
@@ -70,9 +85,11 @@ const Login = () => {
           </div>
         </div>
 
-        <button className=" py-2 w-2/3 bg-secondary rounded-xl mt-3 text-textcolor text-sm md:text-base mb-9">
+        <button onClick={handleSubmit} className="py-2 w-2/3 bg-secondary rounded-xl mt-3 text-textcolor text-sm md:text-base mb-9">
           Login
         </button>
+
+        {error && <p className="text-red-500 text-xs">{error}</p>}
 
         <p className="text-xs md:text-sm text-gray-500 text-center">
           Don't have an account? 
@@ -82,7 +99,7 @@ const Login = () => {
         </p>
         <p className="text-xs md:text-sm text-gray-500 text-center mb-2">
           <span className="text-primary">
-            <button className="cursor-pointer px-2" onClick={forgotPass}> forgot password?</button>
+            <button className="cursor-pointer px-2" onClick={forgotPass}>forgot password?</button>
           </span>
         </p>
       </div>
--- react-frontend/src/component/Auth/Register.jsx
+++ react-frontend/src/component/Auth/Register.jsx
@@ -1,15 +1,31 @@
 import { MdAlternateEmail } from "react-icons/md";
-import { FaFingerprint } from "react-icons/fa";
+import { FaFingerprint, FaUserCircle, FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
-import {FaUserCircle, FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
-
+import { register } from '../../api/api';
 
 const Register = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const togglePasswordView = () => setShowPassword(!showPassword);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [error, setError] = useState(null);
   const navigate = useNavigate();
+
+  const togglePasswordView = () => setShowPassword(!showPassword);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await register(email, password, firstName, lastName);
+      navigate('/');
+      alert("register completed");
+    } catch (error) {
+      setError(error);
+    }
+  };
 
   const handleClick = () => {
     navigate('/');
@@ -17,7 +33,7 @@ const Register = () => {
 
   return (
     <div className="w-full h-screen flex items-center justify-center hero ">
-      <div className="Main w-[90%] w-sm md:w-2/3 lg:w-[40%] p-5 flex-col flex items-center gap-3 rounded-xl shadow-slate-200 shadow-lg child">
+      <div className="Main w-[90%] sm:w-2/3 md:w-[40%] p-5 flex-col flex items-center gap-3 rounded-xl shadow-slate-200 shadow-lg child">
         <h3 className="text-secondary text-base font-bold mt-5">Create your account</h3>
         <p className=" text-primary text-sm">Welcome! Please fill in the details to get started.</p>
         <div className="w-1/2 lg:w-2/3 flex gap-2">
@@ -25,30 +41,31 @@ const Register = () => {
             <AiFillGithub style={{ color: '#181717' }} className="text-lg md:text-xl" />
             <span className="text-sm md:text-base font-medium">GitHub</span>
           </div>
-
           <div className="p-2 md:px-6 lg:px-8 cursor-pointer rounded-xl flex items-center gap-2 icon__btn">
             <AiFillLinkedin style={{ color: '#0077B5' }} className="text-lg md:text-xl" />
             <span className="text-sm md:text-base font-medium">LinkedIn</span>
           </div>
         </div>
 
-        <h3 className="font-lora text-xs md:text-sm px-4 text-gray-500">
-          Or
-        </h3>
+        <h3 className="font-lora text-xs md:text-sm px-4 text-gray-500">Or</h3>
 
         <div className="w-2/3 flex flex-col gap-3">
           <div className="w-full flex items-center gap-2 p-2 rounded-xl icon__btn">
-          <FaUserCircle />
+            <FaUserCircle />
             <input
               type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
               placeholder="First Name"
               className="bg-transparent border-0 w-full outline-none text-sm md:text-base"
             />
           </div>
           <div className="w-full flex items-center gap-2 p-2 rounded-xl icon__btn">
-          <FaUserCircle />
-          <input
+            <FaUserCircle />
+            <input
               type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
               placeholder="Last Name"
               className="bg-transparent border-0 w-full outline-none text-sm md:text-base"
             />
@@ -58,6 +75,8 @@ const Register = () => {
             <MdAlternateEmail />
             <input
               type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="Email address"
               className="bg-transparent border-0 w-full outline-none text-sm md:text-base"
             />
@@ -67,6 +86,8 @@ const Register = () => {
             <FaFingerprint />
             <input
               type={showPassword ? "password" : "text"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder="Password"
               className="bg-transparent border-0 w-full outline-none text-sm md:text-base"
             />
@@ -84,20 +105,21 @@ const Register = () => {
           </div>
         </div>
 
-        <button className=" py-2 w-2/3 bg-secondary rounded-xl mt-3 text-textcolor text-sm md:text-base mb-9">
-          sigin up
+        <button onClick={handleSubmit} className="py-2 w-2/3 bg-secondary rounded-xl mt-3 text-textcolor text-sm md:text-base mb-9">
+          Sign up
         </button>
 
+        {error && <p className="text-red-500 text-xs">{error}</p>}
+
         <p className="text-xs md:text-sm text-gray-500 text-center mb-2">
-           Already have an account? 
+          Already have an account? 
           <span className="text-primary">
-            <button className="cursor-pointer px-2" onClick={handleClick}> Sign in</button>
+            <button className="cursor-pointer px-2" onClick={handleClick}>Sign in</button>
           </span>
         </p>
-
       </div>
     </div>
   );
 }
 
-export default Register
+export default Register;
